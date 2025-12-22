ca532533bdf12b0727bfb12a2602198d264317ff
LuchsHub <robinwagner1910.rw@gmail.com>
2025-12-22 16:30:34 +0100

    Add example code to other_parser

--- parser/other_parser.py
+++ parser/other_parser.py
@@ -1,4 +1,3 @@
-import treelib
 from puml_to_ast import Parser
 
 parser = Parser()
@@ -6,4 +5,17 @@ ast = parser.puml_to_ast()
 
 ast.show()
 
-print(ast.children("coffeemachine"))
+sm_name = ast.get_node(ast.root).tag
+
+for state in ast.children(f"states_in_{sm_name}"):
+    state_node = ast.get_node(state.identifier)
+    print(f"State: {state_node.tag}")
+    if state_node.tag == "[*]":
+        continue
+
+    substate_count = 0
+    for substate in ast.children(f"States_in_{state_node.tag}"):
+        substate_count += 1
+    print(substate_count)
+
+
